43e8965e053bb0f4df843e6ee356ba77c0374690
cesarcastmore <cesar.cast.more@gmail.com>
2018-08-11 14:54:53 -0500

    calculos

--- src/app/cadena-logistica/viajes/camion/camion.component.ts
+++ src/app/cadena-logistica/viajes/camion/camion.component.ts
@@ -16,7 +16,8 @@ export class CamionComponent implements OnInit, AfterViewInit {
   tipos_camiones: any[] = [{
     id: '1TON',
     nombre: 'Camion 1 ton',
-    largo: 2
+    largo: 2,
+    peso: 1000
   }, {
     id: '3.5TON-PF-3MTS',
     nombre: 'Camion 3.5 Toneladas Plataforma 3mts',
@@ -24,51 +25,68 @@ export class CamionComponent implements OnInit, AfterViewInit {
   }, {
     id: '3.5TON-CS-3MTS',
     nombre: 'Camion 3.5 Toneladas Caja Seca 3mts',
-    largo: 3
+    largo: 3,
+    peso: 3500
   }, {
     id: '3.5TON-PF-6MTS',
     nombre: 'Camion 3.5 Toneladas Plataforma 6mts',
-    largo: 6
+    largo: 6,
+    peso: 3500
   }, {
     id: '3.5TON-CS-6MTS',
     nombre: 'Camion 3.5 Toneladas Caja Seca 6mts',
-    largo: 6
+    largo: 6,
+    peso: 3500
+
   }, {
     id: '8TON-R-PF-8MTS',
     nombre: 'Camion Rabon 8 Tonelada Plataforma 8mts',
-    largo: 8
+    largo: 8,
+    peso: 8000
+
   }, {
     id: '8TON-R-CS-8MTS',
     nombre: 'Camion Rabon 8 tonelada Caja Seca 8mts',
-    largo: 8
+    largo: 8,
+    peso: 8000
+
   }, {
     id: '14TON-T-PF-9MTS',
     nombre: 'Camion Torton 14 tonelada Plataforma 9mts',
-    largo: 9
+    largo: 9,
+    peso: 9000
+
   }, {
     id: '14TON-T-CS-9MTS',
     nombre: 'Camion Torton 14 tonelada Caja Seca 9mts',
-    largo: 9
+    largo: 9,
+    peso: 14000
+
   }, {
     id: '20TON-TR-PF-40FT',
     nombre: 'Trailer 20 toneladas Plataforma 40\'',
-    largo: 12.18
+    largo: 12.18,
+    peso: 20000
   }, {
     id: '20TON-TR-CS-40FT',
     nombre: 'Trailer 20 toneladas Caja Seca 40\'',
-    largo: 12.18
+    largo: 12.18,
+    peso: 20000
   }, {
     id: '22TON-TR-PF-48FT',
     nombre: 'Trailer 22 toneladas Plataforma 48\'',
-    largo: 14.63
+    largo: 14.63,
+    peso: 22000
   }, {
     id: '22TON-TR-CS-48FT',
     nombre: 'Trailer 22 toneladas Caja Seca 48\'',
-    largo: 14.63
+    largo: 14.63,
+    peso: 22000
   }, {
     id: '25TON-TR-CS-53FT',
     nombre: 'Trailer 25 toneladas Caja Seca 53\'',
-    largo: 16.15
+    largo: 16.15,
+    peso: 25000
   }];
 
 
@@ -98,28 +116,39 @@ export class CamionComponent implements OnInit, AfterViewInit {
 
   }
 
+
+  public tarima: any;
+  public camion: any;
+
   ngOnInit() {
 
     this.camionForm.valueChanges.subscribe(val => {
 
-      if (val.ancho && val.largo && val.alto && val.cantidad && val.cantidad && val.tipo_camion) {
+      if (val.ancho && val.largo && val.alto && val.cantidad &&
+        val.cantidad && val.tipo_camion && val.peso) {
         console.log(val);
         let tipo_camion: any = this.tipos_camiones.find(tc => tc.id == val.tipo_camion);
 
-        let camion = {
+        this.camion = {
           largo: tipo_camion.largo * this.factor,
           ancho: this.ancho_camion * this.factor,
-          altura: this.altura_camion * this.factor
+          altura: this.altura_camion * this.factor,
+          peso: tipo_camion.peso,
+          peso_consumido: tipo_camion.peso - (val.cantidad * val.peso)
         };
 
-        let tarima = {
+        this.tarima = {
           largo: this.camionForm.value.largo * this.factor,
           ancho: this.camionForm.value.ancho * this.factor,
-          altura: this.camionForm.value.altura * this.factor,
+          altura: this.camionForm.value.altura * this.factor
 
         }
 
-        this.draw(camion, tarima, this.camionForm.value.cantidad);
+
+
+        this.draw(this.camion, this.tarima, this.camionForm.value.cantidad);
+      } else {
+        this.context.clearRect(0, 0, 1000, 1000);
       }
     });
 
@@ -155,7 +184,7 @@ export class CamionComponent implements OnInit, AfterViewInit {
 
       if (posY + tarima.ancho <= camion.ancho) {
 
-        this.context.rect(posX, posY,  tarima.largo, tarima.ancho);
+        this.context.rect(posX, posY, tarima.largo, tarima.ancho);
         this.context.stroke();
 
         posY = posY + tarima.ancho;
@@ -164,7 +193,7 @@ export class CamionComponent implements OnInit, AfterViewInit {
         posY = 0;
         posX = posX + tarima.largo;
 
-        this.context.rect(posX, posY,  tarima.largo, tarima.ancho);
+        this.context.rect(posX, posY, tarima.largo, tarima.ancho);
         this.context.stroke();
         posY = posY + tarima.ancho;
 
